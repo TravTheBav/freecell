79e3bc0c59b42e63933e67a40a9510af3d830c43
tzito <tzito93@gmail.com>
2024-03-11 18:19:41 -0400

    Update readme with basic information

---
+++ deck.py
@@ -0,0 +1,26 @@
+from card import Card
+
+class Deck:
+    """Represents a deck of playing cards. In free cell solitaire the deck is hidden, so 
+    it is primarily used to initialize the cards before they are placed into their starting
+    areas."""
+
+    def __init__(self):
+        self._cards = []
+        self.init_cards()
+
+    def init_cards(self):
+        """Fills the deck with 52 cards."""
+
+        # clubs, spades, diamonds, hearts
+        for suit in range(1, 5):
+            # Ace, 2, 3, ... , Queen, King
+            for value in range(1, 14):
+                card = Card(suit, value)
+
+                self.add_card(card)
+
+    def add_card(self, card):
+        """Adds a card to the top of the deck."""
+
+        self._cards.append(card)
---
+++ game.py
@@ -0,0 +1,8 @@
+class Game:
+    """Represents a game of free cell solitaire. The Game checks for all game logic, such as
+    if a card selection or placement is valid, as well as checking when the game is won. A Game
+    has a deck and various card areas for cards to be placed."""
+
+    def __init__(self):
+        
+        pass
